904e5f08d43bbed314a6d946624a00deef1b0c30
jumitaka <jun22sumi26+github@gmail.com>
2015-04-03 11:31:33 +0900

    fix

--- UpdateFile.ps1
+++ UpdateFile.ps1
@@ -1,12 +1,12 @@
 # 各リポジトリごとの設定内容
 # リモートファイル取得ディレクトリ(git管理ディレクトリ)
-# set repodic=gitdirectory
+# $global:repodic="gitdirectory"
 # コピー元ディレクトリ
-# set #otedic=\\#otedirectory\hoge
+# $global:remotedic="\\remotedirectory\hoge"
 # コピー先ディレクトリ
-# set copydic=hoge
+# $global:copydic="hoge"
 # robocopy引数
-# set roboArg=huga
+# $global:roboArg="huga"
 
 # 設定内容
 $workNameHead = "Work"
@@ -15,19 +15,19 @@ $masterName = "master"
 pushd $global:repodic
 if (-not (Test-Path .git)) {
 	echo "git initを行って下さい。"
-	Read-Host "続けるにはENTERキーを押して下さい" 
+	return
 }
 
 # 日付取得
 $now= Get-Date -Format "yyyyMMddHHmmss"
 
-echo $global:remotedic を取得し、
-echo $global:repodic\$global:copydic の$global:masterName ブランチにコピーします。
+echo "$global:remotedic を取得し、"
+echo "$global:repodic\$global:copydic の$masterName ブランチにコピーします。"
 
 pushd $global:repodic\$global:copydic
 
 # git statusを実行し、変更ファイルがないか確認
-status = $false
+$status = $false
 git status -s | ForEach-Object { $status = $true }
 
 if ($status) {
@@ -35,15 +35,15 @@ if ($status) {
 	git add .
 	git commit -a -m "$workNameHead$now"
 } else {
-	echo 編集中ファイルがないため、%workNameHead% ブランチは作成しません。
+	echo 編集中ファイルがないため、ブランチは作成しません。
 }
 
 git checkout "$masterName"
 
-echo $global:remotedic の最新取得中です...
-robocopy $global:remotedic $global:repodic\$global:copydic /MIR /NP /R:3 /W:3 /xj $roboArg
+echo "$global:remotedic の最新取得中です..."
+robocopy $global:remotedic $global:repodic\$global:copydic /MIR /NP /R:3 /W:3 /xj $global:roboArg
 echo 最新取得完了です。
-echo;
+echo ""
 
 # 最新取得したファイルをコミット
 git add -A
@@ -51,5 +51,6 @@ git commit -a -m "Update $now."
 
 # 作業中ブランチに変更
 if ($status) {
-	git checkout "%workNameHead%%now%"
+	git checkout "$global:workNameHead$now"
 }
+Read-Host "続けるにはENTERキーを押して下さい" 
